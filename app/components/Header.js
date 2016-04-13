3e8a28c52eaef5ddad6b5ff6c40109976fdc7d85
Sudan Vellakovil Kanaka Vel <sudan.kanakavel@maplebots.com>
2016-04-13 15:02:52 +0530

    Added files via upload

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,7 +1,10 @@
 var React = require('react');
 var ReactRouter = require('react-router');
 
-
+var divStyle = {
+  background: "#F5F5DC"
+  
+}; 
 
 var Header = React.createClass({
 
@@ -9,11 +12,12 @@ var Header = React.createClass({
   render: function () {
 
      return (
-
+              <div className= "jumbotron col-sm-6 col-sm-offset-3 text-center" style={divStyle} > 
             
             <h2><font color="orange">CHOOSE YOUR LOAN WISELY </font></h2>
            <h4>An React  App For Calculating Your Monthly Loan Interest in Different Canadian Banks</h4>
            <br /> <br />
+           </div>
 
             )
                      }
